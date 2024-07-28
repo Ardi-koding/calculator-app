8522655af41fc2ad88d6ff260aece044c96b2905
Ardi <ardiantosaputro2001@gmail.com>
2024-07-28 22:05:13 +0700

    javascript

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
-const prompt = require("prompt-sync")();
+// const prompt = require("prompt-sync")();
+
+/* let numbersList = Array.from(numbersHTML); or
+let numbersList = [...numbersHTML]; or 
+let numbersList = [];
+for (let i = 0; i < 10; i++) {
+numbersList.push(numbersHTML[i])
+} */
+
+let numbersHTML = document.getElementsByClassName("numbers");
+let numbersList = [...numbersHTML];
+let numbersData = numbersList.map((e) => e.textContent);
+
+let reset = document.querySelector(".reset");
+
+let showCal = document.querySelector(".show");
+
+numbersList.forEach((element) => {
+	element.addEventListener("click", () => {
+		showCal.textContent = element.textContent;
+		reset.textContent = "C";
+	});
+});
+
+reset.addEventListener("click", () => {
+	if (reset.textContent === "C") {
+		reset.textContent = "AC";
+		showCal.textContent = 0;
+	}
+})
 
 function calculator() {
 	let count = 0;
@@ -6,9 +35,9 @@ function calculator() {
 	while (true) {
 		if (count === 0) {
 			firstInput = Number(prompt("angka pertama: "));
-			while(isNaN(firstInput)) {
-				console.log("Please, enter a valid number")
-				firstInput = Number(prompt("angka pertama: "))
+			while (isNaN(firstInput)) {
+				console.log("Please, enter a valid number");
+				firstInput = Number(prompt("angka pertama: "));
 			}
 		}
 		let userOperator = prompt("masukkan operatormu: ");
@@ -19,8 +48,8 @@ function calculator() {
 		}
 
 		let secondInput = Number(prompt("angka kedua: "));
-		while(isNaN(secondInput)) {
-			console.log("Please enter a valid number")
+		while (isNaN(secondInput)) {
+			console.log("Please enter a valid number");
 			secondInput = Number(prompt("angka kedua"));
 		}
 
@@ -33,7 +62,7 @@ function calculator() {
 		} else if (userOperator == "*") {
 			firstInput *= secondInput;
 			console.log("angka pertama:", firstInput);
-			console.log(typeof(firstInput))
+			console.log(typeof firstInput);
 		} else if (userOperator == "/") {
 			firstInput /= secondInput;
 			console.log("angka pertama:", firstInput);
@@ -46,4 +75,4 @@ function calculator() {
 	}
 }
 
-calculator();
+// calculator();
